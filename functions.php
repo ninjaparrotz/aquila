385a6d7f0015e4b61b5c484ebcf6c5f6cb8f6ab4
Rubby A. Perez Lorenzo <rubbyantonio@gmail.com>
2023-01-24 09:14:42 -0400

    Add Boostrap Library

--- functions.php
+++ functions.php
@@ -21,11 +21,21 @@
 
 function aquila_enqueue_scripts()
 {
+    // Register Styles.
     wp_register_style('styles-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
-    
-    wp_enqueue_style('styles-css');
-    wp_enqueue_script('main-js');
-}
+    wp_register_style('boostrap-css', get_template_directory_uri().'assets/src/library/css/bootstrap.min.css', [], false, 'all');
 
+    // Register Scripts.
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+    wp_register_script('boostrap-js', get_template_directory_uri().'/assets/src/library/js/booststrap.min.js', ['jquery'], false, true);
+    
+    // Enqueue Styles
+    wp_enqueue_style('styles-css');
+    wp_enqueue_style('boostrap-css');
+
+    // Enqueue Scripts
+    wp_enqueue_script('main-js');
+    wp_enqueue_script('boostrap-js');
+
+}
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- header.php
+++ header.php
@@ -16,6 +16,12 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class() ?>>
-    <?php wp_body_open(); ?>
+<body <?php body_class(); ?>>
+
+    <?php
+    if (function_exists('wp_body_open')) {
+        wp_body_open();
+    }
+    ?>
+
     <header>Header</header>
