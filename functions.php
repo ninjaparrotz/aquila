c3651faec0cad7eb22090d4d6fa857995514d9b3
Rubby A. Perez Lorenzo <rubbyantonio@gmail.com>
2023-01-24 08:25:15 -0400

    Add screenshot, styles, javascript files

--- functions.php
+++ functions.php
@@ -1,10 +1,31 @@
 <?php
 
 /**
-  * 
-  * Theme Functions
-  *
-  *@package Aquila
-  */
+ * 
+ * Theme Functions
+ *
+ *@package Aquila
+ */
 
-  
+//  print_r(get_template_directory_uri());
+//  wp_die();
+
+//  print_r(get_stylesheet_uri());
+//  wp_die();
+
+// print_r(filemtime(get_template_directory() . '/style.css'));
+// wp_die();
+
+// wp_register_style('styles-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+// wp_enqueue_style('styles-css');
+
+function aquila_enqueue_scripts()
+{
+    wp_register_style('styles-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+    
+    wp_enqueue_style('styles-css');
+    wp_enqueue_script('main-js');
+}
+
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- header.php
+++ header.php
@@ -8,11 +8,14 @@
 ?>
 <!DOCTYPE html>
 <html lang="<?php language_attributes() ?>">
+
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
+
+<body <?php body_class() ?>>
+    <?php wp_body_open(); ?>
     <header>Header</header>
